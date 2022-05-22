4991f65129b163015cf92f13514139b49a4890c6
AdaSpinal <60679768+AdaSpinal@users.noreply.github.com>
2022-05-21 23:31:06 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -9,3 +9,26 @@ app.get('/', function (req, res) {
 })
 
 app.listen(3000)
+
+function Guardar() {
+  $.ajax({
+      type: 'POST',
+      dataType: "json",
+      url: '/MyController/MyView',
+      traditional: true,
+      data: {
+            array: array
+      },
+      success: function ( data ) {
+        if (data.rstProceso === "true") {
+          console.log( 'Exito: '+ data.rstMensaje );
+        }else if(data.rstProceso === "false"){
+          console.log( 'Algo salió mal '+ data.rstMensaje );
+        }
+      },
+      error: function () {
+          console.log('algo salió mal');
+      }
+  });
+}
+
